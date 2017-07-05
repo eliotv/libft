8ca34bee781c8ce8aa9e5d26bd7bca1c4d63b84a
Eliot vanHeumen <vanman1@gmail.com>
2017-07-04 18:23:21 -0700

    Changed GNL BUFF SIZE to 2000

--- src/get_next_line/get_next_line.h
+++ src/get_next_line/get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: evanheum <evanheum@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/17 17:16:12 by evanheum          #+#    #+#             */
-/*   Updated: 2017/06/12 15:40:06 by evanheum         ###   ########.fr       */
+/*   Updated: 2017/07/03 15:23:04 by evanheum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 1000
+# define BUFF_SIZE 2000
 # define FD_MAX 4864
 # include "../libft/libft.h"
 # include <fcntl.h>
