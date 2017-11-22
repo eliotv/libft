cc7d3162877d67db0271a102cdd8d4b70c58de59
Eliot vanHeumen <evanheum@e1z1r8p1.42.us.org>
2017-11-22 01:56:25 -0800

    color macros

--- megalibft.h
+++ megalibft.h
@@ -6,7 +6,7 @@
 /*   By: evanheum <evanheum@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/12 18:53:55 by evanheum          #+#    #+#             */
-/*   Updated: 2017/06/12 18:55:53 by evanheum         ###   ########.fr       */
+/*   Updated: 2017/11/16 16:51:36 by evanheum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,13 @@
 # include "src/libft/libft.h"
 # include "src/get_next_line/get_next_line.h"
 # include "src/ft_printf/libftprintf.h"
+
+# define RED "\x1b[31m"
+# define GREEN "\x1b[32m"
+# define YELLOW "\x1b[33m"
+# define BLUE "\x1b[34m"
+# define MAGENTA "\x1b[35m"
+# define CYAN "\x1b[36m"
+# define END "\x1b[0m"
 
 #endif
